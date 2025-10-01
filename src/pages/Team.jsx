5057c167181f170303146632f47958302d286bfd
Fadly47 <guagreget124@gmail.com>
2025-10-01 18:28:08 +0700

    fix team.js (gak kelar kelar wkwk)

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -5,7 +5,7 @@ export default function Team() {
       <div className="row">
         <div className="col-md-4">
           <div className="card shadow-sm mb-4">
-            <img src={`${impor.meta.env.BASE_URL}asset/cole-palmer2.jpg`} className="card-img-top" alt="Team Member" />
+            <img src={`${import.meta.env.BASE_URL}asset/cole-palmer2.jpg`} className="card-img-top" alt="Team Member" />
             <div className="card-body">
               <h5 className="card-title">Yogi</h5>
               <p className="card-text">Frontend Developer</p>
@@ -15,7 +15,7 @@ export default function Team() {
 
         <div className="col-md-4">
           <div className="card shadow-sm mb-4">
-            <img src={`${impor.meta.env.BASE_URL}asset/ano-id-new2.jpg`} className="card-img-top" alt="Team Member" />
+            <img src={`${import.meta.env.BASE_URL}asset/ano-id-new2.jpg`} className="card-img-top" alt="Team Member" />
             <div className="card-body">
               <h5 className="card-title">Fadly</h5>
               <p className="card-text">Backend Developer</p>
@@ -25,7 +25,7 @@ export default function Team() {
 
         <div className="col-md-4">
           <div className="card shadow-sm mb-4">
-            <img src={`${impor.meta.env.BASE_URL}asset/pipet-jadii.png`} className="card-img-top" alt="Team Member" />
+            <img src={`${import.meta.env.BASE_URL}asset/pipet-jadii.png`} className="card-img-top" alt="Team Member" />
             <div className="card-body">
               <h5 className="card-title">Philifs</h5>
               <p className="card-text">UI/UX Designer</p>
