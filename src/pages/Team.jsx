e1ab26a31b44930132117cb695258b67eb9ec1e3
Fadly47 <guagreget124@gmail.com>
2025-10-01 17:52:00 +0700

    perbaikan Team.jsx dan lainnya

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -5,7 +5,7 @@ export default function Team() {
       <div className="row">
         <div className="col-md-4">
           <div className="card shadow-sm mb-4">
-            <img src="public/asset/cole-palmer2.jpg" className="card-img-top" alt="Team Member" />
+            <img src="/public/asset/cole-palmer2.jpg" className="card-img-top" alt="Team Member" />
             <div className="card-body">
               <h5 className="card-title">Yogi</h5>
               <p className="card-text">Frontend Developer</p>
